7913c0bac629077ace3153f3a90a304fa58120aa
Isaidhello <a.bye@student.fontys.nl>
2020-04-09 13:48:34 +0200

    NewV1
    Git Switch

---
+++ Assets/Player1_movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1_movement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    //public float speed = 1.0f; //snelheid van ronddraaien
+    
+    // Use this for initialization
+    void Start()
+    {
+        moveSpeed = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {//movement
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal1") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical1") * Time.deltaTime);
+        transform.Rotate(0, Input.GetAxis("Rotate1")*60.0f*Time.deltaTime, 0);
+    }
+}
---
+++ Assets/Player2_movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2_movement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    //public float speed = 1.0f; //snelheid van ronddraaien
+
+    // Use this for initialization
+    void Start()
+    {
+        moveSpeed = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {//movement
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal2") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical2") * Time.deltaTime);
+        transform.Rotate(0, Input.GetAxis("Rotate2")*60.0f*Time.deltaTime, 0);
+    }
+}
