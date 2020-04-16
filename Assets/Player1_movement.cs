a5cc4994699b25f871eecd83f602cebb5037f08e
Isaidhello <a.bye@student.fontys.nl>
2020-04-16 15:25:51 +0200

    kamer1
    kamer 1 toegevoegd

--- Assets/Player1_movement.cs
+++ Assets/Player1_movement.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class Player1_movement : MonoBehaviour
 {
 
-    public float moveSpeed;
-    //public float speed = 1.0f; //snelheid van ronddraaien
-    
-    // Use this for initialization
-    void Start()
-    {
-        moveSpeed = 3f;
-    }
+        public float moveSpeed;
+        //public float speed = 1.0f; //snelheid van ronddraaien
 
-    // Update is called once per frame
-    void Update()
-    {//movement
-        transform.Translate(moveSpeed * Input.GetAxis("Horizontal1") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical1") * Time.deltaTime);
-        transform.Rotate(0, Input.GetAxis("Rotate1")*60.0f*Time.deltaTime, 0);
+        // Use this for initialization
+        void Start()
+        {
+            moveSpeed = 3f;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {//movement
+            transform.Translate(moveSpeed * Input.GetAxis("Horizontal1") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical1") * Time.deltaTime);
+            transform.Rotate(0, Input.GetAxis("Rotate1") * 60.0f * Time.deltaTime, 0);
+        }
     }
-}
