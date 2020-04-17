fd39b47b5aa107ecaf030f9cbac6d2d8138705f5
Furkan Öztürk <furkan___01@live.nl>
2020-04-17 15:27:48 +0200

    Game over scherm
    Game over trigger event aangemaakt op enemy dat triggert zodra hij een player aanraakt.

--- Assets/EnemyFollow.cs
+++ Assets/EnemyFollow.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class EnemyFollow : MonoBehaviour
 {
@@ -17,5 +18,15 @@ public class EnemyFollow : MonoBehaviour
     {
         //set destination van navmeshagent dat op enemy zit
         gameObject.GetComponent<NavMeshAgent>().SetDestination(ThePlayer.transform.position);
+    }
+
+
+        void OnTriggerEnter(Collider collider) //methode voor het triggeren van een event
+    {
+        if(collider.gameObject.tag == "Player") //als het object in contact komt met een object dat getagged is als "Player"
+        {
+            print("Player dead"); //print deze tekst als bevestiging
+            SceneManager.LoadScene(2);
+        }
     }
 }
