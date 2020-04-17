32ba445f4a211c0937702ce07a2370d5ad906a66
Furkan Öztürk <furkan___01@live.nl>
2020-04-17 16:43:02 +0200

    Key opent deur
    Door het object in klaslokaal 1 te pakken kun je wel door de deur heen (wordt dan verwijderd)

--- Assets/pickup_object.cs
+++ Assets/pickup_object.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class pickup_object : MonoBehaviour
 {
+    public bool canOpen = false;
     void OnTriggerEnter(Collider collider) //methode voor het triggeren van een event
     {
         if(collider.gameObject.tag == "Player") //als het object in contact komt met een object dat getagged is als "Player"
         {
             print("Item picked up"); //print deze tekst als bevestiging
             Destroy(gameObject); //vernietig het object
+            canOpen = true;
         }
     }
 }
---
+++ Assets/player_open.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_open : MonoBehaviour
+{
+    public pickup_object po;
+
+    void OnTriggerEnter(Collider collider) //methode voor het triggeren van een event
+    {
+        if(collider.gameObject.tag == "Player" && po.canOpen == true) //als het object in contact komt met een object dat getagged is als "Player"
+        {
+            print("Door opened"); //print deze tekst als bevestiging
+            Destroy(gameObject); //vernietig het object
+        }
+    }
+}
