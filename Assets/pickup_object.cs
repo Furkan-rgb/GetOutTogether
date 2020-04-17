f2a1ae646a2a801e207a5cf5837d768f25c0f85a
Furkan Öztürk <furkan___01@live.nl>
2020-04-17 14:11:43 +0200

    Rotatie fix + event object geplaatst
    Uncontrollable rotatie van speler gefixt zodra het in contact kwam met objecten in de omgeving. Object geplaatst dat event triggert in klaslokaal 1

---
+++ Assets/pickup_object.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup_object : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider) //methode voor het triggeren van een event
+    {
+        if(collider.gameObject.tag == "Player") //als het object in contact komt met een object dat getagged is als "Player"
+        {
+            print("Item picked up"); //print deze tekst als bevestiging
+            Destroy(gameObject); //vernietig het object
+        }
+    }
+}
