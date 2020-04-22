36af8e863553fa4601b5c331a9bec57e3596463f
Furkan Öztürk <furkan___01@live.nl>
2020-04-22 22:11:56 +0200

    Audio manager toegevoegd
    Audio manager zorgt nu voor achtergrond muziek en verandert het zodra de key is gepakt.

--- Assets/pickup_object.cs
+++ Assets/pickup_object.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class pickup_object : MonoBehaviour
 {
     public bool canOpen = false;
+
+        void Start()
+    {
+
+    }
+
     void OnTriggerEnter(Collider collider) //methode voor het triggeren van een event
     {
         if(collider.gameObject.tag == "Player") //als het object in contact komt met een object dat getagged is als "Player"
         {
             print("Item picked up"); //print deze tekst als bevestiging
-            Destroy(gameObject); //vernietig het object
+            //Destroy(gameObject); //vernietig het object
+            gameObject.SetActive(false);
             canOpen = true;
         }
     }
